3e2708e7c524756eed508f5a8687418bf9a83ce1
Gabuvi7 <gabriel.uviedo@sarsy-srl.com>
2019-06-24 23:14:57 -0300

    5/5 CRUD en Laravel 5.6 - Actualizar y Eliminar records
    Utilizo postman para realizar los post, get, put, delete.
    
    http://jsonviewer.stack.hu ---> Lo utilizo para formatear json.
    
    POST -- INSERTA
    GET -- OBTIENE
    PUT -- ACTUALIZA
    DELETE -- BORRA

--- apiRestCRUD/app/Http/Controllers/MovieController.php
+++ apiRestCRUD/app/Http/Controllers/MovieController.php
@@ -14,17 +14,10 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
-    }
+        //echo "HOLA!!";
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $movies =  Movie::get();
+        echo json_encode($movies);
     }
 
     /**
@@ -35,51 +28,52 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        //Esta funcion debe guardar los nuevos usuarios.
+        //print_r($request->all()); //print_r puede tomar objetos completos.
+        $movie = new Movie(); //Creo un nuevo objeto, para poder acceder a los param e insertarlos en la movie.
+        $movie->name = $request->input('name');
+        $movie->description = $request->input('description');
+        $movie->year = $request->input('year');
+        $movie->genre = $request->input('genre');
+        $movie->duration = $request->input('duration');
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Movie $movie)
-    {
-        //
-    }
+        $movie->save(); //guardo el obj.
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Movie $movie)
-    {
-        //
-    }
+        echo json_encode($movie); // De esta forma guardo el objeto en formato json.
 
+    }
+    
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Movie  $movie
+     * @param  \App\Movie  $movie_id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Movie $movie)
+    public function update(Request $request, $movie_id)
     {
-        //
+        $movie = Movie::find($movie_id); //Con esto referencio al id que esta en la DB, de esta forma NO creo un obj nuevo. 
+        $movie->name = $request->input('name');
+        $movie->description = $request->input('description');
+        $movie->year = $request->input('year');
+        $movie->genre = $request->input('genre');
+        $movie->duration = $request->input('duration');
+
+        $movie->save(); //guardo el obj.
+
+        echo json_encode($movie); // De esta forma guardo el objeto en formato json.
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Movie  $movie
+     * @param  \App\Movie  $movie_id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Movie $movie)
+    public function destroy($movie_id)
     {
-        //
+        $movie = Movie::find($movie_id);
+        $movie->delete();
     }
 }
